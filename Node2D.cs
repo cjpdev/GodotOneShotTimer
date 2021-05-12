5e1e6b61de741e2d7f1b5da58a6e76b8e0c1d3b2
Chay Palton <10560512+cjpdev@users.noreply.github.com>
2021-05-12 19:58:17 +0100

    CallDeferred
    CallDeferred

--- Node2D.cs
+++ Node2D.cs
@@ -7,7 +7,7 @@ public class Node2D : Godot.Node2D
     {
         OneShotTimer one1 = new OneShotTimer(this, 2, nameof(TestFunc));
         OneShotTimer one2 = new OneShotTimer(this, 4, nameof(TestFunc));
-        OneShotTimer one3 = new OneShotTimer(this, 6, nameof(TestFunc));
+        OneShotTimer one3 = new OneShotTimer(this, 6, nameof(TestFunc), false);
         OneShotTimer oneA1 = new OneShotTimer(this, 2, nameof(TestFunc2));
     }
 
